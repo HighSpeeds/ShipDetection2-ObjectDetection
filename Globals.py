ed0248a7b8e9a63f11cd105f7372c971ecb3089b
Neptune726 <lawrence.liu.us@gmail.com>
2022-01-09 20:12:19 -0800

    Added Two files

---
+++ Globals.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  9 20:09:27 2022
+
+@author: Lawrence
+"""
+
+Data_Dir="D:\Data" #I saved the data to another hardrive
+                   #replace this with wherever you have saved the files
+                   
